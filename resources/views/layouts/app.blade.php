eadae26e0e33b8d65cdf83daaa9ea28bf2bf5752
sub zero <cinkyal35@gmail.com>
2024-07-22 20:36:04 +0630

    make sideBar & navigationBar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="dark">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,64 +17,28 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+    <main id="app" class="container-fluid">
+        @auth
+            <div class="row">
+                <!--            navigation menu bar start -->
+                <div class="col-sm-3 col-lg-2 d-none d-sm-block vh-100 bg-black overflow-scroll scrollbar">
+                    @include('layouts.sideBar')
                 </div>
-            </div>
-        </nav>
+                <!--            navigation menu bar end-->
 
-        <main class="py-4">
+                <!--            navigation bar start-->
+                <div class="col-12 col-sm-9 col-lg-10">
+                    @include('layouts.navigationBar')
+                    @yield('content')
+                </div>
+                <!--            navigation bar end-->
+            </div>
+        @endauth
+
+        @guest
+            @include('layouts.navigationBar')
             @yield('content')
-        </main>
-    </div>
+        @endguest
+    </main>
 </body>
 </html>
---
+++ resources/views/layouts/sideBar.blade.php
@@ -0,0 +1,82 @@
+<div class="side-bar">
+    <nav class="navbar navbar-dark mb-3 bg-black sticky-top">
+        <a class="navbar-brand w-100 text-danger my-2 fs-4 fw-bold" href="{{ url('/') }}">
+            <i class="bi bi-strava fs-2"></i> STRAVA
+        </a>
+    </nav>
+    <ul class="nav nav-fill flex-column m-2">
+
+        <li class="text-white-50 fw-bold mb-2">
+            <small>DATA MANAGEMENT</small>
+        </li>
+
+        <li class="nav-item fs-6 px-2 py-3">
+            <a href="#" class="nav-link p-0 text-decoration-none text-white text-start">
+                <i class="me-2 bi bi-grid-1x2-fill"></i> Dashboard
+            </a>
+        </li>
+        <li class="nav-item fs-6 px-2 py-3">
+            <a href="#" class="nav-link p-0 text-decoration-none text-white text-start">
+                <i class="me-2 bi bi-window-stack"></i> To Blog
+            </a>
+        </li>
+        <li class="nav-item fs-6 px-2 py-3">
+            <a href="#" class="nav-link p-0 text-decoration-none text-white text-start">
+                <i class="me-2 bi bi-bell-fill"></i> Notification
+            </a>
+        </li>
+        <li class="nav-item fs-6 px-2 py-3">
+            <a href="#" class="nav-link p-0 text-decoration-none text-white text-start">
+                <i class="me-2 bi bi-gear-fill"></i> Settings
+            </a>
+        </li>
+
+
+        <li class="text-white-50 fw-bold mb-2 mt-4">
+            <small>BLOG MANAGEMENT</small>
+        </li>
+
+        <li class="nav-item fs-6 px-2 py-3">
+            <a href="#" class="nav-link p-0 text-decoration-none text-white text-start">
+                <i class="me-2 bi bi-chat-quote-fill"></i> Blog List & Edit
+            </a>
+        </li>
+        <li class="nav-item fs-6 px-2 py-3">
+            <a href="#" class="nav-link p-0 text-decoration-none text-white text-start">
+                <i class="me-2 bi bi-plus-circle-fill"></i> Create
+            </a>
+        </li>
+
+
+        <li class="text-white-50 fw-bold mb-2 mt-4">
+            <small>USER MANAGEMENT</small>
+        </li>
+
+        <li class="nav-item fs-6 px-2 py-3">
+            <a href="#" class="nav-link p-0 text-decoration-none text-white text-start">
+                <i class="me-2 bi bi-people-fill"></i> User List & Edit
+            </a>
+        </li>
+
+        <li class="nav-item fs-6 px-2 py-3">
+            <a href="#" class="nav-link p-0 text-decoration-none text-white text-start">
+                <i class="me-2 bi bi-chat-fill"></i> Chatting
+            </a>
+        </li>
+
+        <li class="text-white-50 fw-bold mb-2 mt-4">
+            <small>ACCOUNT MANAGEMENT</small>
+        </li>
+
+        <li class="nav-item fs-6 px-2 py-3">
+            <a href="#" class="nav-link p-0 text-decoration-none text-white text-start">
+                <i class="me-2 bi bi-person-circle"></i> Profile
+            </a>
+        </li>
+        <li class="nav-item fs-6 px-2 py-3">
+            <a href="#" class="nav-link p-0 text-decoration-none text-white text-start">
+                <i class="me-2 bi bi-person-fill-lock"></i> SignOut
+            </a>
+        </li>
+    </ul>
+</div>
