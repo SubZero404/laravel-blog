0173d2426d2fba964c5b3977bbd194bd8c14b02f
sub zero <cinkyal35@gmail.com>
2024-08-25 22:22:30 +0630

    modifine photo migrations, when post del photos also del-cascadeOnDelete

--- database/migrations/2024_08_17_135915_create_photos_table.php
+++ database/migrations/2024_08_17_135915_create_photos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('photos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('post_id');
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
